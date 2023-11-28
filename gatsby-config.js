ea9e4602cb16d969ae145d4e28ff5ce72568245b
“Adi91108” <adityasen911@gmail.com>
2023-11-28 16:03:24 +0530

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Density-trading-app`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
+
